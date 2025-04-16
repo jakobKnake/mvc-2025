ff7b37391ccbc64ae2cbc28b3cccc963d3d1b97b
Jakob <jahl24@student.bth.se>
2025-04-16 15:44:00 +0200

    Add session debugging functionality
    Implement session route to display session content. Create Twig template for visualization of session data and
    viewing CardHand and CardGraphic objects

--- src/Controller/CardGameController.php
+++ src/Controller/CardGameController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Card\CardGraphic;
+use App\Card\CardHand;
 
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -26,5 +28,32 @@ class CardGameController extends AbstractController
     public function home(): Response
     {
         return $this->render('card/home.html.twig');
+    }
+
+    /**
+     * @Route(
+     *      "/session",
+     *      name="session"
+     * )
+     */
+    #[Route("/session", name: "session")]
+    public function session(Request $request, SessionInterface $session): Response
+    {
+        $card = new CardGraphic;
+        $card->draw();
+        $hand = new CardHand;
+        $hand->add($card);
+        $session->set('card', $card);
+        $session->set('hand', $hand);
+
+        $cardSession = $session->get('card');
+        $handSession = $session->get('hand');
+        $data = $session->all();
+
+        return $this->render('card/session.html.twig', [
+            'card' => $cardSession,
+            'hand' => $handSession,
+            'data' => $data
+        ]);
     }
 }
