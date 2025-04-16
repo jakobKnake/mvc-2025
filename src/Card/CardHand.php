22ae874d322ae119fd5b9980dd4e2dcbad8c2330
Jakob <jahl24@student.bth.se>
2025-04-16 14:24:17 +0200

    Started on CardHand

---
+++ src/Card/CardHand.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Card;
+
+use App\Card\Card;
+
+/**
+ * Class CardHand.
+ * Representing a hand of playing cards.
+ */
+class CardHand
+{
+    /**
+     * @var array $cards Array of Card objects in the hand.
+     */
+    private $hand = [];
+
+    /**
+     * Add card to the hand.
+     * @param Card $card The card to add.
+     * @return void
+     */
+    public function add(Card $card): void
+    {
+        $this->hand[] = $card;
+    }
+
+    /**
+     * Get all cards in the hand.
+     * @return array Array of the cards in the hand.
+     */
+    public function getCards(): array
+    {
+        return $this->hand;
+    }
+
+    /**
+     * Get the numbers of cards in the hand.
+     * @return int The amount of cards.
+     */
+    public function getNumberCards(): int
+    {
+        return count($this->hand);
+    }
+
+}
