7e8e956eacc1360a40310ca94860ac263ec9f8ab
Jakob <jahl24@student.bth.se>
2025-05-22 20:05:34 +0200

    Exclude path php fixer in Scrutinizer config

--- docs/metrics/classes.js
+++ docs/metrics/classes.js
@@ -298,7 +298,7 @@ var classes = [
         "afferentCoupling": 1,
         "efferentCoupling": 0,
         "instability": 0,
-        "numberOfUnitTests": 5,
+        "numberOfUnitTests": 6,
         "violations": {}
     },
     {
