bccdb4095ef5c480a2bc7ef3d6e5e0bc5ba1f305
Jakob <jahl24@student.bth.se>
2025-05-29 13:41:52 +0200

    Add Create in Controller and template (CRUD)
    - Add template for loggin with form and route
    - Add template for create user with form and route almost fully functional
    - Add bonus images
    - Add Avatars png files for user to select later
    - Add routes in project base for 'Logga in' and 'Skapa Konto'

--- src/Controller/ProjController.php
+++ src/Controller/ProjController.php
@@ -3,6 +3,14 @@
 namespace App\Controller;
 
 use App\Card\GameLogic;
+use App\Entity\History;
+use App\Entity\User;
+use App\Repository\HistoryRepository;
+use App\Repository\UserRepository;
+
+
+use Doctrine\Persistence\ManagerRegistry;
+
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
@@ -23,6 +31,38 @@ class ProjController extends AbstractController
     public function logInGet(): Response
     {
         return $this->render('proj/log_in.html.twig');
+    }
+
+    // Här kommer post sen
+    //#[Route("/proj/loggin", name: "logg_in_post", methods: ['POST'])]
+    //public function logInPost(Request $request, SessionInterface $session): Response
+    //{}
+
+    #[Route("/proj/create", name: "create_user", methods: ['GET'])]
+    public function createGet(): Response
+    {
+        return $this->render('proj/create.html.twig');
+    }
+    #[Route("/proj/create", name: "create_user_post", methods: ['POST'])]
+    public function createPost(ManagerRegistry $doctrine, 
+    Request $request, SessionInterface $session): Response
+    {
+        $projEntityManager = $doctrine->getManager('project');
+
+        $username = $request->request->get('username');
+        $password = $request->request->get('password');
+        $bonus = $request->request->get('bonus-offer');
+
+        $hashedPassword = password_hash($password, PASSWORD_DEFAULT);
+
+        $user = new User();
+        $user->setUsername($username);
+        $user->setPassword($hashedPassword);
+        $user->setProfilePic('profile.png');
+
+        $projEntityManager->persist($user);
+
+        $projEntityManager->flush($user);
     }
 
 }
